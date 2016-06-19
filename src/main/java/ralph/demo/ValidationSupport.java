fb1b3416a7a6b8e8556999f60137b4c41acf1b23
ralph <ridder@dds.nl>
2016-06-19 11:28:40 +0200

    Refactor validation.

---
+++ src/main/java/ralph/demo/ValidationSupport.java
@@ -0,0 +1,21 @@
+package ralph.demo;
+
+import java.util.List;
+
+/**
+ * Created by ralph on 6/19/16.
+ */
+public class ValidationSupport {
+
+
+    public static final boolean validateGiven(String value, String validationMessage, List<String> validationMessages) {
+        return validate(value != null && !value.trim().isEmpty(), validationMessage, validationMessages);
+    }
+
+    public static final boolean validate(boolean expression, String validationMessage, List<String> validationMessages) {
+        if (!expression) {
+            validationMessages.add(validationMessage);
+        }
+        return true;
+    }
+}
--- src/main/java/ralph/demo/account/AccountForm.java
+++ src/main/java/ralph/demo/account/AccountForm.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static ralph.demo.ValidationSupport.validate;
+import static ralph.demo.ValidationSupport.validateGiven;
+
 /**
  * Created by ralph on 6/19/16.
  */
@@ -43,44 +46,32 @@ public class AccountForm {
     public Optional<List<String>> validateInput() {
         List<String> validationMessages = new ArrayList<>();
         validateUsernameGiven(validationMessages);
+        validatePassword(validationMessages);
+        return validationMessages.isEmpty() ? Optional.empty() : Optional.of(validationMessages);
+    }
+
+    void validatePassword(List<String> validationMessages) {
         boolean passwordGiven = validatePasswordGiven(validationMessages);
         boolean passwordCheckGiven = validatePasswordCheckGiven(validationMessages);
         if (passwordCheckGiven && passwordGiven) {
             validatePasswordAndCheckEqual(validationMessages);
         }
-        return validationMessages.isEmpty() ? Optional.empty() : Optional.of(validationMessages);
     }
 
-    private boolean validateUsernameGiven(List<String> validationMessages) {
-        String userName = getUsername();
-        return validate(userName != null && !userName.trim().isEmpty(), "username.missing", validationMessages);
+    boolean validateUsernameGiven(List<String> validationMessages) {
+        return validate(username != null && !username.trim().isEmpty(), "username.missing", validationMessages);
     }
 
-    private boolean validatePasswordGiven(List<String> validationMessages) {
-        String password = getPassword();
+    boolean validatePasswordGiven(List<String> validationMessages) {
         return validateGiven(password, "password.missing", validationMessages);
     }
 
-    private boolean validatePasswordCheckGiven(List<String> validationMessages) {
-        String password = getPasswordCheck();
-        return validateGiven(password, "passwordCheck.missing", validationMessages);
+    boolean validatePasswordCheckGiven(List<String> validationMessages) {
+        return validateGiven(passwordCheck, "passwordCheck.missing", validationMessages);
     }
 
-    private boolean validatePasswordAndCheckEqual(List<String> validationMessges) {
-        String password = getPassword();
-        String passwordCheck = getPasswordCheck();
+    boolean validatePasswordAndCheckEqual(List<String> validationMessges) {
         return validate(passwordCheck.equals(password), "passwordCheck.doesnotmatchpassword", validationMessges);
-    }
-
-    private boolean validateGiven(String value, String validationMessage, List<String> validationMessages) {
-        return validate(value != null && !value.trim().isEmpty(), validationMessage, validationMessages);
-    }
-
-    private boolean validate(boolean expression, String validationMessage, List<String> validationMessages) {
-        if (!expression) {
-            validationMessages.add(validationMessage);
-        }
-        return true;
     }
 
 
