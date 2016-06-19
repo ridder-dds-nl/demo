49df481244544c312c65294e9054befa4cc91248
ralph <ridder@dds.nl>
2016-06-19 13:51:36 +0200

    Spring requires only one public static void main

--- src/main/java/ralph/demo/MailApp.java
+++ src/main/java/ralph/demo/MailApp.java
@@ -11,7 +11,7 @@ import java.util.Properties;
  */
 public class MailApp {
 
-    public static void main(String... args) {
+    public static void main() {
 
         MailSender mailSender = createMailSender();
 
